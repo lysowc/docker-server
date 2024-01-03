ac864959c9008a5f3a15a7b70010ac4d013528b0
lysowc <sora@Sora>
2024-01-03 18:44:53 +0800

    update script

--- init.sh
+++ init.sh
@@ -5,7 +5,13 @@ then
    exit
 fi
 
-cd /usr/local/
+dir=/home/sora/server
+
+if [ ! -d "$dir" ]; then
+  mkdir -p $dir
+fi
+
+cd $dir
 mkdir -p mysql redis php nginx
 
 #配置nginx
@@ -14,26 +20,28 @@ mkdir logs
 chmod 777 logs
 cp -r $1/docker-server/nginx/conf.d ./
 cp $1/docker-server/nginx/nginx.conf ./
-cd ..
 
 #配置mysql
-cd mysql
-mkdir -p data logs
-chmod 777 logs
-cp -r $1/docker-server/mysql/conf.d ./
+cd $dir/mysql
+cp -r $1/docker-server/mysql/my.cnf ./
 cp -r $1/docker-server/mysql/docker-entrypoint-initdb.d/ ./
+cp -r $1/docker-server/mysql/data/ ./
+cp -r $1/docker-server/mysql/bin/ ./
+cp -r $1/docker-server/mysql/logs/ ./
+cp -r $1/docker-server/mysql/slow/ ./
 
 
 #配置redis
-cd /usr/local/redis/
-mkdir -p logs data
+cd $dir/redis
+mkdir -p logs
 cp $1/docker-server/redis/redis.conf ./
-chmod 777 logs/ data/
+cp -r $1/docker-server/redis/data ./
+chmod 777 -R logs
 
 #配置php
-cd /usr/local/php/
+cd $dir/php
 cp $1/docker-server/php/php8/fpm/php.ini ./
-mkdir php-fpm.d
+mkdir php-fpm.d log
 cd php-fpm.d/
 cp $1/docker-server/php/php8/fpm/zz-docker.conf ./
 
